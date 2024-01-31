3b7d750c616f327d6077d830ce0eafbd39b3c4f0
Milena Maia Araújo <milenamaiaaraujo@192.168.100.11>
2024-01-31 17:07:24 -0300

    ✨ project creation

---
+++ Confeite/Confeite/ConfeiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfeiteApp.swift
+//  Confeite
+//
+//  Created by Milena Maia Araújo on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ConfeiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Confeite/Confeite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Confeite
+//
+//  Created by Milena Maia Araújo on 31/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
